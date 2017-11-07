fb07b5193673e9b3d8204712031e27cf98df229a
Yavor Georgiev <jimbolp@gmail.com>
2017-11-07 17:13:14 +0200

    Added connection to the database. It's showing the Name of the article ot the customer if any!

--- CalculateControlDigit/CalculateControlDigit/MainForm.Designer.cs
+++ CalculateControlDigit/CalculateControlDigit/MainForm.Designer.cs
@@ -31,12 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.enterNumberTxtBox = new System.Windows.Forms.TextBox();
             this.calculateBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.resultLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // enterNumberTxtBox
             // 
-            this.enterNumberTxtBox.Location = new System.Drawing.Point(42, 34);
+            this.enterNumberTxtBox.Location = new System.Drawing.Point(158, 35);
             this.enterNumberTxtBox.Name = "enterNumberTxtBox";
             this.enterNumberTxtBox.Size = new System.Drawing.Size(100, 20);
             this.enterNumberTxtBox.TabIndex = 0;
@@ -45,13 +47,36 @@
             // 
             // calculateBtn
             // 
-            this.calculateBtn.Location = new System.Drawing.Point(42, 77);
+            this.calculateBtn.Location = new System.Drawing.Point(115, 76);
             this.calculateBtn.Name = "calculateBtn";
-            this.calculateBtn.Size = new System.Drawing.Size(100, 23);
+            this.calculateBtn.Size = new System.Drawing.Size(71, 23);
             this.calculateBtn.TabIndex = 1;
             this.calculateBtn.Text = "Изчисли";
             this.calculateBtn.UseVisualStyleBackColor = true;
             this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoEllipsis = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(0, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(406, 147);
+            this.label2.TabIndex = 2;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 76);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Копирай";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // resultLabel
             // 
@@ -59,18 +84,20 @@
             this.resultLabel.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.resultLabel.ForeColor = System.Drawing.Color.Red;
-            this.resultLabel.Location = new System.Drawing.Point(0, 119);
+            this.resultLabel.Location = new System.Drawing.Point(0, 138);
             this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(192, 42);
-            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Size = new System.Drawing.Size(406, 34);
+            this.resultLabel.TabIndex = 4;
             this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(192, 161);
+            this.ClientSize = new System.Drawing.Size(406, 319);
             this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.calculateBtn);
             this.Controls.Add(this.enterNumberTxtBox);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -85,6 +112,8 @@
 
         private System.Windows.Forms.TextBox enterNumberTxtBox;
         private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label resultLabel;
     }
 }
--- CalculateControlDigit/CalculateControlDigit/MainForm.cs
+++ CalculateControlDigit/CalculateControlDigit/MainForm.cs
@@ -5,10 +5,21 @@ using System.Windows.Forms;
 namespace CalculateControlDigit
 {
     public partial class MainForm : Form
-    {
+    {        
+        private bool _connectionEstablished = false;
+        LibraCentralEntities db = null;
         public MainForm()
         {
             InitializeComponent();
+            try
+            {
+                db = new LibraCentralEntities();
+                _connectionEstablished = true;
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("No Connection to the database!", "Error!");
+            }
         }
 
         private void enterNumberTxtBox_TextChanged(object sender, EventArgs e)
@@ -16,6 +27,54 @@ namespace CalculateControlDigit
             if (!isValid(enterNumberTxtBox.Text))
             {
                 enterNumberTxtBox.Text = "";
+                resultLabel.Text = "";
+            }
+            else
+            {
+                Calculate();
+                GetArticleFromDB(enterNumberTxtBox.Text);
+            }
+        }
+
+        private void GetArticleFromDB(string text)
+        {
+            if (Int32.TryParse(text, out int intVal))
+            {
+                string article = "";
+                string customer = "";
+                try
+                {
+                    Article a = db.Articles.Find(intVal);
+                    if(a == null)
+                    {
+                        article = "Няма артикул с този номер";
+                    }
+                    else
+                    {
+                        article += a.CyrName;
+                    }
+                }
+                catch (Exception)
+                {
+                    article = "Няма артикул с този номер";
+                }
+                try
+                {
+                    Customer c = db.Customers.Find(intVal);
+                    if (c == null)
+                    {
+                        customer = "Няма клиент с този номер";
+                    }       
+                    else
+                    {
+                        customer += c.Name;
+                    }
+                }
+                catch (Exception)
+                {
+                    customer = "Няма клиент с този номер";
+                }
+                label2.Text = "Артикул: " + Environment.NewLine + article + Environment.NewLine + "Клиент:" + Environment.NewLine + customer;
             }
         }
 
@@ -36,9 +95,22 @@ namespace CalculateControlDigit
 
         private void calculateBtn_Click(object sender, EventArgs e)
         {
+            Calculate();
+        }
+
+        private void enterNumberTxtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                calculateBtn_Click(sender, e);
+            }
+        }
+
+        private void Calculate()
+        {
             int.TryParse(enterNumberTxtBox.Text, out int number);
-            int result = (7 * ((number / 1) % 10) + 6 * ((number / 10) % 10) 
-                + 5 * ((number / 100) % 10) + 4 * ((number / 1000) % 10) 
+            int result = (7 * ((number / 1) % 10) + 6 * ((number / 10) % 10)
+                + 5 * ((number / 100) % 10) + 4 * ((number / 1000) % 10)
                 + 3 * ((number / 10000) % 10) + 2 * ((number / 100000) % 10));
             if ((result % 11) == 10)
                 number = number * 10 + 0;
@@ -47,11 +119,16 @@ namespace CalculateControlDigit
             resultLabel.Text = number.ToString();
         }
 
-        private void enterNumberTxtBox_KeyDown(object sender, KeyEventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            if(e.KeyCode == Keys.Enter)
+            if (isValid(enterNumberTxtBox.Text))
             {
-                calculateBtn_Click(sender, e);
+                Clipboard.SetText(resultLabel.Text);
+                MessageBox.Show("Запаметено е в клипборда!", "Clipboard");
+            }
+            else
+            {
+                MessageBox.Show("Въведеното число не е валидно!", "Clipboard", icon: MessageBoxIcon.Warning, buttons: MessageBoxButtons.OK, defaultButton: MessageBoxDefaultButton.Button1);
             }
         }
     }
